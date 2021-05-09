c4ad4e8e6510c0290c93f99ae33ea4ea37563344
André Palma <andremarquespalma@gmail.com>
2021-05-09 11:55:15 +0100

    Add Micro Otimizations

--- src/main/java/Legionellosis.java
+++ src/main/java/Legionellosis.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -6,44 +7,34 @@ public class Legionellosis {
     private final MultidirGraph graph;
     //[home,maxDistance]
     private final int[] sick;
+    private final int numSick;
 
     public Legionellosis(MultidirGraph graph, int[] sick) {
         this.graph = graph;
         this.sick = sick;
+        numSick = sick.length / 2;
     }
 
     public String res() {
         int[] hits = new int[graph.size()];
-        for (int i = 0; i < sick.length / 2; i++) {
-            boolean[] processed = new boolean[graph.size()];
-            explore(sick[i * 2], processed, sick[i * 2 + 1]);
-            for (int b = 0; b < processed.length; b++) {
-                if (processed[b])
-                    hits[b]++;
-            }
+        boolean[] processed = new boolean[graph.size()];
+        for (int i = 0; i < numSick; i++) {
+            Arrays.fill(processed, false);
+            explore(hits, processed, sick[i * 2], sick[i * 2 + 1]);
         }
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < graph.size(); i++) {
-            if (hits[i] == sick.length / 2)
-                output.append(i + 1).append(" ");
-        }
-
-        if (output.length() == 0) {
-            return "0";
-        } else {
-            output.setLength(output.length() - 1);
-        }
-        return output.toString();
+        return formatOutput(hits);
     }
 
-    private void explore(int current, boolean[] processed, int currentLevel) {
+    private void explore(int[] hits, boolean[] processed, int current, int currentLevel) {
         Queue<Integer> q = new LinkedList<>();
         q.add(current);
-        processed[current] = true;
         int toNextLevel = 1;
         int toNextLevelDec = 0;
         while (!q.isEmpty()) {
             current = q.poll();
+            if (!processed[current]) {
+                hits[current]++;
+            }
             processed[current] = true;
             MultidirGraph.NodeIterator iter = graph.iteratorAt(current);
             while (iter.hasNext()) {
@@ -60,4 +51,20 @@ public class Legionellosis {
             }
         }
     }
+
+    private String formatOutput(int[] hits) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < hits.length; i++) {
+            if (hits[i] == numSick)
+                output.append(i + 1).append(' ');
+        }
+
+        if (output.length() == 0) {
+            return "0";
+        } else {
+            output.setLength(output.length() - 1);
+        }
+        return output.toString();
+    }
+
 }
